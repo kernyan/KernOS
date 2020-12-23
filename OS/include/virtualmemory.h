53b638e09bd1746ceb217072d09a5d1bd9fb2e2c
kernyan <kernyan@users.noreply.github.com>
2020-12-23 11:27:29 -0500

    Page fault support on full 4GB range

--- OS/include/virtualmemory.h
+++ OS/include/virtualmemory.h
@@ -63,11 +63,94 @@ namespace VM
   void ParseMultibootMemoryMap(const multiboot_info_t &MultibootInfoPtr);
   void FlushTLB(const uint32_t Fault_Address);
 
+  struct PhysPage
+  {
+     uint32_t m_PhysicalAddress;
+     size_t   m_PDE;
+     size_t   m_PTE;
+     size_t   m_Offset;
+
+     PhysPage(const uint32_t PhysAddr) :
+        m_PhysicalAddress (PhysAddr),
+        m_PDE             (PhysAddr >> 22 & 0x3FF),
+        m_PTE             (PhysAddr >> 12 & 0x3FF),
+        m_Offset          (PhysAddr       & 0xFFF)
+     {
+     }
+  };
+
+  enum class PAGE_ATTR
+  {
+     MAPPED  = 0,
+     UNMAPPED
+  };
+
+  struct PhysicalPageAllocator
+  {
+     uint32_t m_BaseAddr; // start of memory region
+     size_t   m_Size;     // size of free memory region
+     size_t   m_Offset;   // allocated memory
+
+     uint32_t GetFreePage()
+     {
+        if (m_Offset + PG_SIZE <= m_Size)
+        {
+           uint32_t FreePage = m_BaseAddr + m_Offset;
+
+           m_Offset += PG_SIZE;
+
+           return FreePage;
+        }
+
+        kprintf("Failed to get free page\n");
+        Hang();
+     }
+
+     void ReturnPage(const uint32_t VAddr); // not yet implemented
+
+     void Initialize(uint32_t BaseAddr, size_t Size)
+     {
+        Size = (Size / PG_SIZE) * PG_SIZE; // round down
+
+        if (BaseAddr & 0xFFF)
+        {
+           kprintf("Warning: Physical memory is not 4KB aligned at %h\n", BaseAddr);
+        }
+
+        m_BaseAddr = BaseAddr;
+        m_Size     = Size;
+        m_Offset   = 0;
+     }
+
+     PAGE_ATTR GetAttr(const uint32_t VAddr)
+     {
+        //if (m_Bitmap.test(VAddr >> 12 & 0xFFFFF))
+        //{
+        //   return PAGE_ATTR::MAPPED; // TODO implement later
+        //}
+        //else
+        {
+           return PAGE_ATTR::UNMAPPED;
+        }
+     }
+  };
+
+  struct PageAttributes
+  {
+     PhysPage m_PhysPage;
+
+     PageAttributes(const uint32_t PhysAddr) :
+        m_PhysPage (PhysAddr)
+     {
+     }
+  };
+
   class VMManager
   {
-    public:
-      void MapPageTable(uint32_t VAddr);
-      void Initialize(multiboot_info_t &BootMemoryMap);
+     public:
+        void MapPageTable(uint32_t VAddr);
+        void Initialize(multiboot_info_t &BootMemoryMap);
+        PhysicalPageAllocator           m_PhyPageAllocator; // TODO make private
   };
 
   extern VMManager S;
--- OS/kernel/arch/x86/virtualmemory.cpp
+++ OS/kernel/arch/x86/virtualmemory.cpp
@@ -33,7 +33,9 @@ namespace VM // virtual memory
     *  @param PageDirectory
     *  @param PageTable
     */
-   void MapPageTable(const size_t Idx, uint32_t PageDirectory[PD_SIZE], uint32_t PageTable[PT_SIZE])
+   void MapPageTable(const size_t Idx, 
+                     uint32_t PageDirectory[PD_SIZE], 
+                     uint32_t PageTable[PT_SIZE])
    {
        for (size_t i = 0; i < PT_SIZE; ++i)
        {
@@ -49,6 +51,31 @@ namespace VM // virtual memory
                               )
                             );
    }
+
+   void SetPage(const PageAttributes PageAttr, 
+                uint32_t PageDirectory[PD_SIZE], 
+                uint32_t PageTable[PT_SIZE])
+   {
+      size_t PDE     = PageAttr.m_PhysPage.m_PDE;
+      size_t PTE     = PageAttr.m_PhysPage.m_PTE;
+      PageTable[PTE] = (PTE * PG_SIZE)
+                       | ( DWord<PTA::R>()
+                         | DWord<PTA::P>()
+                         );
+
+      if (PageDirectory[PDE] == (uint32_t) PageTable)
+      {
+         kprintf("PageDirectoryEntry already exists. Access rights not updated\n");
+      }
+      else
+      {
+         PageDirectory[PDE] = ( (uint32_t) PageTable // top 20 bits is addr of page frame
+                              | ( DWord<PDA::R>()    // bottom 12 bits are access rights
+                                | DWord<PDA::P>()
+                                )
+                              );
+      }
+   };
 
    /*! @brief Set cr3 to page directory, and turn on paging
     *  @param PageDirectory
@@ -75,77 +102,84 @@ namespace VM // virtual memory
 
    extern "C" void FaultPageHandler()
    {
-       uint32_t Fault_Address;
-       asm volatile("mov %%cr2, %0" : "=r" (Fault_Address));
+      uint32_t Fault_Address;
+      asm volatile("mov %%cr2, %0" : "=r" (Fault_Address));
 
-       if (  Fault_Address >= 4 * MB
-          && Fault_Address  < 8 * MB
-          )
-       {
-         VM::S.MapPageTable(Fault_Address);
-         VM::FlushTLB(Fault_Address);
-       }
-       else
-       {
-           kprintf("Page fault handler only works for 4MB - 8MB range for now\n");
-           Hang();
-       }
-
-       kprintf("Page fault handler called\n");
+      kprintf("Page fault handler called\n");
+      VM::S.MapPageTable(Fault_Address);
    }
 
    void ParseMultibootMemoryMap(const multiboot_info_t& MultibootInfo)
    {
-     kprintf("\nMemory map from bootloader\n");
+      kprintf("\nMemory map from bootloader\n");
 
-     for (auto* MMap = (multiboot_memory_map_t*) MultibootInfo.mmap_addr;
-         (unsigned long) MMap < MultibootInfo.mmap_addr + MultibootInfo.mmap_length;
-         MMap = (multiboot_memory_map_t*) ((unsigned long) MMap + MMap->size + sizeof(MMap->size))
-         )
-     {
-       const auto BaseAddr = (uint32_t) (MMap->addr);
-       const auto Length   = (uint32_t) (MMap->len);
+      uint32_t BaseAddrMax = 0;
+      size_t   SizeMax     = 0;
 
-       kprintf("BaseAddr: %h Length: %h\n", BaseAddr, Length);
-     }
+      for (auto* MMap = (multiboot_memory_map_t*) MultibootInfo.mmap_addr;
+          (unsigned long) MMap < MultibootInfo.mmap_addr + MultibootInfo.mmap_length;
+          MMap = (multiboot_memory_map_t*) ((unsigned long) MMap + MMap->size + sizeof(MMap->size))
+          )
+      {
+        const auto BaseAddr = (uint32_t) (MMap->addr);
+        const auto Length   = (uint32_t) (MMap->len);
+
+        if (SizeMax < Length)
+        {
+           BaseAddrMax = BaseAddr;
+           SizeMax     = Length;
+        }
+
+        kprintf("BaseAddr: %h Length: %h\n", BaseAddr, Length);
+      }
+
+      if (SizeMax > PG_SIZE)
+      {
+        S.m_PhyPageAllocator.Initialize(BaseAddrMax, SizeMax);
+      }
    }
 
    void FlushTLB(const uint32_t Fault_Address)
    {
-     asm volatile( // flush tlb
-     "invlpg %0"
-     :
-     : "m"(*(char*) Fault_Address)
-     : "memory"
-     );
+      asm volatile( // flush tlb
+      "invlpg %0"
+      :
+      : "m"(*(char*) Fault_Address)
+      : "memory"
+      );
    }
 
    void VMManager::Initialize(multiboot_info_t &BootMemoryMap)
    {
-     ParseMultibootMemoryMap(BootMemoryMap);
-     VM::InitializePageDirectory(VM::kernel_page_directory);
-     VM::MapPageTable(0, VM::kernel_page_directory, VM::pagetable0);
-     //VM::MapPageTable(1, VM::kernel_page_directory, VM::pagetable1);
-     VM::InstallPaging(VM::kernel_page_directory);
+      ParseMultibootMemoryMap(BootMemoryMap);
+      VM::InitializePageDirectory(VM::kernel_page_directory);
+      VM::MapPageTable(0, VM::kernel_page_directory, VM::pagetable0);
+      VM::MapPageTable(1, VM::kernel_page_directory, VM::pagetable1);
+      VM::InstallPaging(VM::kernel_page_directory);
    }
 
    void VMManager::MapPageTable(uint32_t VAddr)
    {
-      if (  VAddr >= 4 * MB
-         && VAddr  < 8 * MB
-         )
+      if (VAddr < 8 * MB)
       {
-        kprintf("Page fault handler called\n");
-      }
-      else
-      {
-        kprintf("Page fault handler only works for 4MB - 8MB range for now\n");
+        kprintf("Lower 8MB is always mapped. Page fault here is logical fault\n");
         Hang();
       }
 
       kprintf("VMManger mapping virtual address %h\n", VAddr);
-      VM::MapPageTable(1, VM::kernel_page_directory, VM::pagetable1);
-      VM::FlushTLB(VAddr);
+
+      switch (m_PhyPageAllocator.GetAttr(VAddr))
+      {
+      case PAGE_ATTR::MAPPED:
+         return;
+      case PAGE_ATTR::UNMAPPED:
+         auto FreePhyPageAddr = m_PhyPageAllocator.GetFreePage();
+         kprintf("FreePhyPageAddr: %h\n", FreePhyPageAddr);
+         PageAttributes PageAttr {VAddr};
+         SetPage(PageAttr,  kernel_page_directory, (uint32_t*) FreePhyPageAddr);
+         FlushTLB(VAddr);
+         break;
+      }
    }
 
 } // namespace VM
