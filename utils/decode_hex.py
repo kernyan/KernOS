f826daa08ed2813eae27174989ae827f65ab7242
kernyan <kernyan@users.noreply.github.com>
2025-01-11 23:10:45 -0500

    utilities

---
+++ utils/decode_hex.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python3
+
+import argparse
+
+def parse_options():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("hex", type=str)
+    return parser.parse_args()
+
+def decode(hex_str: str):
+    return bin(int(hex_str, 16))
+
+def active_bits(bin_str: str):
+    active_bits = []
+    for i, e in enumerate(reversed(bin_str[2:])):
+        if e == "1":
+            active_bits.append(i)
+    return sorted(active_bits, reverse=True)
+
+def main():
+    args = parse_options()
+    bin_str = decode(args.hex)
+    print(bin_str)
+    print(active_bits(bin_str))
+
+if __name__ == "__main__":
+    main()
+
